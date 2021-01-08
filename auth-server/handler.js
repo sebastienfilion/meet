554138019c8c948a4ae7ef68295d4399f9733bc6
Mitch Zilberg2 <mitchell@Mitchells-MacBook-Pro.local>
2021-01-08 12:20:18 -0500

    added function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,16 +1,9 @@
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
-/**
- * SCOPES allows you to set access levels; this is set to readonly for now because you don't have access rights to
- * update the calendar yourself. For more info, check out the SCOPES documentation at this link: https://developers.google.com/identity/protocols/oauth2/scopes
- */
+
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
-/**
- * Credentials are those values required to get access to your calendar. If you see “process.env” this means
- * the value is in the “config.json” file. This is a best practice as it keeps your API secrets hidden. Please remember to add “config.json” to your “.gitignore” file.
- */
 const credentials = {
   client_id: process.env.CLIENT_ID,
   project_id: process.env.PROJECT_ID,
@@ -86,3 +79,55 @@ module.exports.getAccessToken = async (event) => {
     };
   });
 };
+
+module.exports.getCalendarEvents = async (event) => {
+  
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+    
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`); //code
+  oAuth2Client.setCredentials({
+    access_token
+  });
+
+  return new Promise((resolve, reject) => {
+    calendar.events.list({
+      calendarId: calendar_id,
+      auth: oAuth2Client,
+      timeMin: new Date().toISOString(),
+      maxResults: 32,
+      singleEvents: true,
+      orderBy: "startTime",
+    },
+    (error, response) => {
+      if (error) {
+        reject (error);
+      } else {
+        resolve (response);
+      }
+    }
+    );
+  })
+  .then ((results) => {
+    return {
+      statusCode: 200,
+      // headers: {
+      //   'Access-Control-Allow-Origin': '*'
+      // },
+      body: JSON.stringify({
+        events: results.data.items
+      })
+    };
+  })
+  .catch ((err) => {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err),
+    };
+  });
+};
+
