58160a52047e8444227fe81b3ebada24232df353
Mitch Zilberg2 <mitchell@Mitchells-MacBook-Pro.local>
2021-01-14 15:19:18 -0500

    added tests

--- src/EventList.js
+++ src/EventList.js
@@ -5,7 +5,7 @@ class EventList extends Component {
     render() {
         const {events} = this.props;
         return (
-            <ul classname='EventList'>
+            <ul className='EventList'>
                 {this.props.events.map(event => 
                     <li key={event.id}>
                         <Event event={event} />
