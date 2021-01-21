0fa90bff53bbac654c1e96fe373d43610734d680
Mitch Zilberg2 <mitchell@Mitchells-MacBook-Pro.local>
2021-01-21 16:11:01 -0500

    yml and handler change

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -150,40 +150,11 @@ module.exports.getAuthURL = async () => {};
 module.exports.getAccessToken = async (event) => {};
 module.exports.getCalendarEvents = async (event) => {};
 module.exports.ping = () => {
-  try {
-    const { google } = require("googleapis");
-    const OAuth2 = google.auth.OAuth2;
-    const calendar = google.calendar("v3");
-    const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
-    const credentials = {
-      client_id: process.env.CLIENT_ID,
-      project_id: process.env.PROJECT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      calendar_id: process.env.CALENDAR_ID,
-      auth_uri: "https://accounts.google.com/o/oauth2/auth",
-      token_uri: "https://oauth2.googleapis.com/token",
-      auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-      redirect_uris: ["https://mitchellzilberg.github.io/meet/"],
-      javascript_origins: ["https://mitchellzilberg.github.io", "http://localhost:8080", "http://localhost:3000", "http://localhost"],
-    };
-    const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
-    const oAuth2Client = new google.auth.OAuth2(
-      client_id,
-      client_secret,
-      redirect_uris[0]
-    );
-    return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
-      body: JSON.stringify({ client_id, client_secret })
-    }
-  } catch (error) {
-    
-    return {
-      statusCode: 500,
-      body: error.message
-    }
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: "OK"
   }
 };
